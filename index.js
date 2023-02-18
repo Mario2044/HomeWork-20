34a513509691ffeb7c18ccfc18af03a1e528191b
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-18 20:13:21 +0200

    Update js file

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-//створити функцію яка буде приймати матиматичну дію '+' ,'-', т.д.
-//й буде повертати функцію яка в незалежності від кількості аргументів буде повертати
-//стрінгу з перечисленням цих аргументів і їх матиматичним перетворенням
-function funcWithMath (operator) {
-    return function () {
+const setMath = (operator) => {
+    return (...numbers) => {
+      const result = numbers.reduce((acc, cur) => {
+        return operator === '+' ? acc + cur :
+               operator === '-' ? acc - cur :
+               operator === '*' ? acc * cur :
+               operator === '/' ? acc / cur :
+               acc;
+      });
+      const operationString = numbers.join(` ${operator} `);
+      return `${operationString} = ${result}`;
     }
-}
+};
+  
+const setNumbers = (...numbers) => {
+    return (operator) => {
+      const result = numbers.reduce((acc, cur) => {
+        return operator === '+' ? acc + cur :
+               operator === '-' ? acc - cur :
+               operator === '*' ? acc * cur :
+               operator === '/' ? acc / cur :
+               acc;
+      });
+      const operationString = numbers.join(` ${operator} `);
+      return `${operationString} = ${result}`;
+    }
+};
+  
+const funcWithMath = setMath('-');
+console.log(funcWithMath(10, 2, 3)); 
+console.log(funcWithMath(3, 1)); 
+
+const whatDo = setNumbers(10, 2, 3);
+console.log(whatDo('-')); 
+console.log(whatDo('+')); 
+
+const whatDo_1 = setNumbers(3, 1);
+console.log(whatDo_1('-'));
+console.log(whatDo_1('+')); 
